4d6859ba36cbba5c61bc860752f3d4be9cbe53b7
CiccaGuru <robyciccarelli.rc@gmail.com>
2016-07-10 12:23:59 +0200

    Create classes for students and interface to manage them

--- js/admin.js
+++ js/admin.js
@@ -409,6 +409,25 @@ function modificaCorso(id){
 	});
 }
 
+function eliminaClasse(id){
+	var posting = $.post('../include/eliminaClasse.php',{"id":id});
+	posting.done(function(data){
+		if(data == "SUCCESS"){
+			aggiornaListaSezioni();
+		}
+		else{
+			Materialize.toast('<i class="material-icons red-text" style="margin-right:0.2em">error</i> Si è verificato un errore. Controlla la console', 4000);
+			console.log(data);
+		}
+	});
+}
+function aggiornaListaSezioni(){
+	var posting = $.post('../include/elencoSezioni.php',{1:1});
+	posting.done(function(data){
+		$("#dettagliSezioni").html('	<li class="collection-header blue-text center"><h4 class="condensed light">ELENCO SEZIONI</h4></li>'+data);
+	});
+}
+
 function applicaModificaOre(idCorso){
 	console.log("CIAO");
 	$("#modal-ore .row").each(function(i){
@@ -442,6 +461,7 @@ function applicaModificaOre(idCorso){
 (function($){
 	$(function(){
 		aggiornaListaDocenti();
+		aggiornaListaSezioni();
 		$('.modal-trigger').leanModal();
 		$("#aggiungi-docente").submit(function(e){
 			e.preventDefault();
@@ -468,6 +488,26 @@ function applicaModificaOre(idCorso){
 
 				});
 			}
+		});
+
+		$("#aggiungiClasse").on("click", function(){
+			var posting = $.post(
+				'../include/creaClasse.php',
+				{
+					"classe":$("#selezionaClasseStudente").val(),
+					"sezione":$("#sezione").val()
+				}
+			);
+			posting.done(function(data){
+				if(data == "SUCCESS"){
+					 Materialize.toast('Classe creata con successo!', 4000);
+					 aggiornaListaSezioni();
+				}
+				else{
+					 Materialize.toast('<i class="material-icons red-text" style="margin-right:0.2em">error</i> Si è verificato un errore, controlla la console', 4000);
+					 console.log(data);
+				}
+			});
 		});
 
 		$("#aggiungi-studente").submit(function(e){
